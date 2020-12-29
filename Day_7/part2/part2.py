8f992c28932348bfc285942d068d468fc2ef1e9c
andrius <aplienius@gmail.com>
2020-12-29 23:00:40 +0200

    seven day part 2

--- Day_7/part2/part2.py
+++ Day_7/part2/part2.py
@@ -1,26 +1,67 @@
 import re
 
-with open("../part1/input.txt", 'r') as f:
+with open("input.txt", 'r') as f:
     content = f.read()
     line_list = [re.sub('bags|bag','', line) for line in content.split('\n')]
-    print(line_list)
-#  color: color1,color2
-my_color = "shiny gold"
 
-dic_lst= []
+my_color = "shiny gold"
+mydict = {}
 
 for line in line_list:
     color_dic = {}
-    contains ={}
     a = line.split('contain')
-    key = a[0]
+    key = a[0].strip()
     value = a[1]
-    #value = re.sub('\d', '',a[1] )
-    #print(value.replace('.', ''))
     color_dic[key]=[element.strip() for element in value.replace('.', '').split(',') ]
-    dic_lst.append(color_dic)
-    #print(type(color_dic[key]))
-    # patt = re.sub('bags','', a[0])
-    # print(patt.rstrip(), len(patt.rstrip()), len(patt))
-print(dic_lst)
+    mydict.update(color_dic)
+
+
+def check_color_in_decs_key(color:str,my_dic:dict):
+    for key ,value in my_dic.items():
+        if color == key:
+            return my_dic[color]
+        else:
+            return False
+
+def calc_bags(my_color):
+    total_count = 0
+    contain=mydict[my_color]
+    if contain:
+        for item in contain :
+            new_color = re.sub(r'\d+', '', item).strip()
+            if re.findall(r'\d+', item):
+                num = int(re.findall(r'\d+', item)[0])
+                total_count = total_count + (num + num * calc_bags(new_color))
+
+        return total_count
+
+
+sk = calc_bags(my_color)
+print(sk)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
